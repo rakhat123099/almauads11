fbde37eccfc8c7e40fa7916251b0f9475f81b564
rakhat <seitkazyrahat@gmial.com>
2022-11-30 13:34:46 +0600

    setting py alowed host

--- almauads/settings.py
+++ almauads/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n_2vqn9j0^2z6^o49u+fd#kh1fm6n9b4rvpkx1p7)mci#e+f^^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["rakhat12309.pythonanywhere.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
